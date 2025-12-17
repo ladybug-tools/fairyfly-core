d98f7a3aa163162476a335c053123ffa8f36ea46
Chris Mackey <chris@ladybug.tools>
2025-12-16 16:52:22 -0800

    feat(shape): Add a class for fairyfly Shape

--- fairyfly/writer/model.py
+++ fairyfly/writer/model.py
@@ -2,5 +2,5 @@
 
 Note to developers:
 Use this module to extend fairyfly's Model writer for new extensions.
-(eg. adding `thmz` to this module adds the method `Model.to.thmz`)
+(eg. adding `therm` to this module adds the method `Model.to.therm`)
 """
