51dce0e98d8e2662be295ce1bde15ed252cf805e
Chris Mackey <chris@ladybug.tools>
2025-12-16 16:52:22 -0800

    feat(shape): Add a class for fairyfly Shape

--- fairyfly/writer/boundary.py
+++ fairyfly/writer/boundary.py
@@ -2,5 +2,5 @@
 
 Note to developers:
 Use this module to extend fairyfly's Boundary writer for new extensions.
-(eg. adding `thmz` to this module adds the method `Boundary.to.thmz`)
+(eg. adding `therm` to this module adds the method `Boundary.to.therm`)
 """
--- fairyfly/writer/shape.py
+++ fairyfly/writer/shape.py
@@ -2,5 +2,5 @@
 
 Note to developers:
 Use this module to extend fairyfly's Shape writer for new extensions.
-(eg. adding `thmz` to this module adds the method `Shape.to.thmz`)
+(eg. adding `therm` to this module adds the method `Shape.to.therm`)
 """
